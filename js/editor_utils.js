fed72dc0e3aa21a18a73abf6e6272ffa2b832b88
jluini <jluini@gmail.com>
2020-08-19 19:54:49 -0300

    Initial commit

---
+++ js/editor_utils.js
@@ -0,0 +1,84 @@
+function startEditor(editorId, initialData) {
+  let config = {
+    holder: editorId,
+    autofocus: true,
+    tools: {
+      header: Header,
+      list: {
+        class: List,
+        inlineToolbar: true,
+      },
+      image: SimpleImage,
+    },
+  };
+  
+  if(initialData) {
+    config.data = initialData;
+  }
+  
+  return new EditorJS(config);
+}
+
+function getEditorData(callback) {
+  return editor.save().then((outputData) => {
+    callback(outputData);
+  }).catch((error) => {
+    callback({error});
+  });
+}
+
+function parseEditorData(data) {
+  return {
+    html: blocksToHtml(data.blocks) //`<h1>Este sería el &lt;resultado&gt; (${data.time})</h1>`
+  };
+}
+
+/******************************************/
+
+function blocksToHtml(blocks) {
+  return blocks.map(blockToHtml).join("\n");
+}
+
+function blockToHtml(block) {
+  if(blockRenderers[block.type]) {
+    return blockRenderers[block.type](block.data);
+  } else {
+    console.warn(`Unknown block type '${block.type}'`);
+    return "";
+  }
+}
+
+blockRenderers = {
+  paragraph(data) {
+    return `<p>${data.text}</p>`;
+  },
+  header(data) {
+    return `<h${data.level}>${data.text}</h${data.level}>`;
+  },
+  list(data) {
+    let tag = getListTag(data.style);
+    if(!tag) {
+      console.warn(`Unknown list style '${data.style}'`);
+      return "";
+    }
+    
+    return `<${tag}>${getListContent(data.items)}</${tag}>`
+  }
+};
+
+function getListTag(style) {
+  switch(style) {
+    case "ordered":
+      return "ol";
+    case "unordered":
+      return "ul";
+    default:
+      return false;
+  }
+}
+
+function getListContent(items) {
+  return items.map((item) => {
+    return `<li>${item}</li>`;
+  }).join("\n");
+}
